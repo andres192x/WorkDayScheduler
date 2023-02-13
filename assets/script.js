ba31d2194a4f6603e1320e883b8e7e5e07140e66
Andres Chaux <git@github.com:andres192x>
2023-02-12 21:15:56 -0500

    redo script.js modified index and styling, added more hours to the schedule

--- assets/script.js
+++ assets/script.js
@@ -5,40 +5,58 @@ $(document).ready(function () {
 
     $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+        var times = $(this).parent().attr("id");
 
-        localStorage.setItem(time, text);
+        localStorage.setItem(times, text);
     })
 
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
-    $("#hour18 .description").val(localStorage.getItem("hour18"));
+    $("#hr7 .description").val(localStorage.getItem("hr7"));
+    $("#hr8 .description").val(localStorage.getItem("hr8"));
+    $("#hr9 .description").val(localStorage.getItem("hr9"));
+    $("#hr10 .description").val(localStorage.getItem("hr10"));
+    $("#hr11 .description").val(localStorage.getItem("hr11"));
+    $("#hr12 .description").val(localStorage.getItem("hr12"));
+    $("#hr13 .description").val(localStorage.getItem("hr13"));
+    $("#hr14 .description").val(localStorage.getItem("hr14"));
+    $("#hr15 .description").val(localStorage.getItem("hr15"));
+    $("#hr16 .description").val(localStorage.getItem("hr16"));
+    $("#hr17 .description").val(localStorage.getItem("hr17"));
+    $("#hr18 .description").val(localStorage.getItem("hr18"));
+    $("#hr19 .description").val(localStorage.getItem("hr19"));
+    $("#hr20 .description").val(localStorage.getItem("hr20"));
+    $("#hr21 .description").val(localStorage.getItem("hr21"));
+    $("#hr22 .description").val(localStorage.getItem("hr22"));
+    $("#hr23 .description").val(localStorage.getItem("hr23"));
+    $("#hr24 .description").val(localStorage.getItem("hr24"));
 
     $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
 
-    function hourTracker() {
-        //current # of hrs
-        var currentHour = moment().hour(); //moment.js
-
-        // loop time blocks
-        $(".time-block").each(function () {
-            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+    function hrTrack() {
+        
+        var presentHr = moment().hour(); 
+        for (var i =1;i<=24;i++){
+            var register= $("#hr"+i)
             
-            if (blockHour < currentHour) {
+            if (i<presentHr){
+                register.addClass("bg-secondary")
+            }
+
+            if (i==presentHr){
+            register.addClass("bg-danger")
+            }
+        }
+
+        
+        $(".time-block").each(function () {
+            var timeBox = parseInt($(this).attr("id").split("hour")[1]);
+            
+            if (timeBox < presentHr) {
                 $(this).addClass("past");
                 $(this).removeClass("future");
                 $(this).removeClass("present");
             }
-            else if (blockHour === currentHour) {
+            else if (timeBox === presentHr) {
                 $(this).removeClass("past");
                 $(this).addClass("present");
                 $(this).removeClass("future");
@@ -51,7 +69,6 @@ $(document).ready(function () {
         })
     }
 
-    hourTracker(); //run function again
-
+    hrTrack(); 
 })
 
